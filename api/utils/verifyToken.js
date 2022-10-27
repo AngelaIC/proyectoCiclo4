6feba47aa306e6a5a2515e303e0d391073986912
AngelaIC <kimypunchos@gmail.com>
2022-10-26 21:22:18 -0500

    start

--- api/utils/verifyToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-import { createError } from "../utils/error.js";
-
-export const verifyToken = (req, res, next) => {
-    const token = req.cookies.acces_token;
-    if(!token){
-        return next(createError(401, "Lo sentimos, usted no está ahutorizad@"))
-    }
-}
